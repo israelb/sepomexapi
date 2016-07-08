e5d117960182fc049d5c34a3c41cec2f404add2b
Israel Barba <israelb.aceves@gmail.com>
2016-07-08 16:12:14 -0500

    asterisk

--- lib/sepomex_api/endpoint.ex
+++ lib/sepomex_api/endpoint.ex
@@ -38,7 +38,7 @@ defmodule SepomexApi.Endpoint do
     key: "_sepomex_api_key",
     signing_salt: "oh0r4P3H"
 
-  plug CORSPlug, [origin: "http://localhost:3000"]
+  plug CORSPlug, [origin: "*"]
 
   plug SepomexApi.Router
 end
